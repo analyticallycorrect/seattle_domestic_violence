a29bdafe96a79e8d0b9b1d208c3699508c68aeb1
Joseph Armes <armesjoe@gmail.com>
2018-12-06 12:37:42 -0800

    create initial app route

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import numpy as np
 import pandas as pd
-from Filter_Data import Run_Recommender
-from Group_Runs import GroupRuns
-import Mapping_Functions.Map_Routes as mapfun
 from io import BytesIO
 import folium 
 
 from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__, static_url_path="")
+
+#csv files with forecast data and ratings for heatmap
+city_predictions = pd.read_csv('data/city_predictions.csv')
+neighborhood_ratings = pd.read_csv('data/neighborhood_ratings.csv')
+neighborhood_predictions = pd.read_csv('data/neighborhood_predictions.csv')
+
+
+@app.route('/') #landing page
+def settle_dv():
+    return render_template('index.html')
