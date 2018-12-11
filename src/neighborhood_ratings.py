6627613360cfbfcbe3a2e787edc7df53180267ee
Joseph Armes <armesjoe@gmail.com>
2018-12-11 10:33:04 -0800

    fix bugs in neighborhood_ratings function

--- src/neighborhood_ratings.py
+++ src/neighborhood_ratings.py
@@ -1,10 +1,13 @@
 import pandas as pd
-import nump as np
+import numpy as np
 
 
-def neighborhood_ratings(predictions_df, column_names):
-    neighborhood_predicted_counts = pd.DataFrame(predictions_df.T, columns=column_names)
+def neighborhood_ratings(predictions_df, features, column_names):
+    neighborhood_predicted_counts  = predictions_df.T
+    neighborhood_predicted_counts.columns = column_names
+    neighborhood_predicted_counts = neighborhood_predicted_counts.join(features['date'])
     neighborhood_means = neighborhood_predicted_counts.mean()
     neighborhood_std = neighborhood_predicted_counts.std()
     ratings = (((neighborhood_means - neighborhood_predicted_counts.drop('date', axis=1)) / (neighborhood_std*2)) + .5)
+    ratings = ratings.join(features['date'])
     return ratings
