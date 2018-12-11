f96a484ac3fd8438cd717af9cb3a885b6560159a
Joseph Armes <armesjoe@gmail.com>
2018-12-10 18:00:51 -0800

    run black for pep8 formatting on neighbohood_ratings.py

--- src/neighborhood_ratings.py
+++ src/neighborhood_ratings.py
@@ -1,11 +1,14 @@
-import pandas as import pd
+import pandas as pd
 import nump as np
 
 
 def neighborhood_ratings(predictions_df, column_names):
     neighborhood_predicted_counts = pd.DataFrame(predictions_df.T, columns=column_names)
-    neighborhood_predicted_counts = neighborhood_predicted_counts.join(results['date'])
+    neighborhood_predicted_counts = neighborhood_predicted_counts.join(results["date"])
     neighborhood_means = neighborhood_predicted_counts.mean()
     neighborhood_std = neighborhood_predicted_counts.std()
-    ratings = (((neighborhood_means - neighborhood_predicted_counts.drop('date', axis=1)) / (neighborhood_std*2)) + .5)
+    ratings = (
+        (neighborhood_means - neighborhood_predicted_counts.drop("date", axis=1))
+        / (neighborhood_std * 2)
+    ) + 0.5
     return ratings
